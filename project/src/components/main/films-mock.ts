41177c10c00b71e101908aab389f70ed96d85c5a
Дмитрий Васильев <orckuss@gmail.com>
2021-10-03 03:02:12 +0300

    module2-task2: Create SmallFilmCard component

---
+++ project/src/components/main/films-mock.ts
@@ -0,0 +1,105 @@
+import { PreviewFilm } from '../../types/film';
+
+// ToDo: Delete mock on stage of integration with server
+export const FILMS_MOCK: Array<PreviewFilm> = [
+  {
+    id: 1,
+    previewImage: 'img/fantastic-beasts-the-crimes-of-grindelwald.jpg',
+    name: 'Fantastic Beasts: The Crimes of Grindelwald',
+  },
+  {
+    id: 2,
+    previewImage: 'img/bohemian-rhapsody.jpg',
+    name: 'Bohemian Rhapsody',
+  },
+  {
+    id: 3,
+    previewImage: 'img/macbeth.jpg',
+    name: 'Macbeth',
+  },
+  {
+    id: 4,
+    previewImage: 'img/aviator.jpg',
+    name: 'Aviator',
+  },
+  {
+    id: 5,
+    previewImage: 'img/we-need-to-talk-about-kevin.jpg',
+    name: 'We need to talk about Kevin',
+  },
+  {
+    id: 6,
+    previewImage: 'img/what-we-do-in-the-shadows.jpg',
+    name: 'What We Do in the Shadows',
+  },
+  {
+    id: 7,
+    previewImage: 'img/revenant.jpg',
+    name: 'Revenant',
+  },
+  {
+    id: 8,
+    previewImage: 'img/johnny-english.jpg',
+    name: 'Johnny English',
+  },
+  {
+    id: 9,
+    previewImage: 'img/shutter-island.jpg',
+    name: 'Shutter Island',
+  },
+  {
+    id: 10,
+    previewImage: 'img/pulp-fiction.jpg',
+    name: 'Pulp Fiction',
+  },
+  {
+    id: 11,
+    previewImage: 'img/no-country-for-old-men.jpg',
+    name: 'No Country for Old Men',
+  },
+  {
+    id: 12,
+    previewImage: 'img/snatch.jpg',
+    name: 'Snatch',
+  },
+  {
+    id: 13,
+    previewImage: 'img/moonrise-kingdom.jpg',
+    name: 'Moonrise Kingdom',
+  },
+  {
+    id: 14,
+    previewImage: 'img/seven-years-in-tibet.jpg',
+    name: 'Seven Years in Tibet',
+  },
+  {
+    id: 15,
+    previewImage: 'img/midnight-special.jpg',
+    name: 'Midnight Special',
+  },
+  {
+    id: 16,
+    previewImage: 'img/war-of-the-worlds.jpg',
+    name: 'War of the Worlds',
+  },
+  {
+    id: 17,
+    previewImage: 'img/dardjeeling-limited.jpg',
+    name: 'Dardjeeling Limited',
+  },
+  {
+    id: 18,
+    previewImage: 'img/orlando.jpg',
+    name: 'Orlando',
+  },
+  {
+    id: 19,
+    previewImage: 'img/mindhunter.jpg',
+    name: 'Mindhunter',
+  },
+  {
+    id: 20,
+    previewImage: 'img/midnight-special.jpg',
+    name: 'Midnight Special',
+  },
+];
---
+++ project/src/types/film.ts
@@ -0,0 +1,6 @@
+// ToDO: Supplement the type at the stage of integration with the server
+export type PreviewFilm = {
+  readonly id: number;
+  readonly name: string;
+  readonly previewImage: string;
+}
