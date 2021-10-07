6a34ee636cb9523468af8e5114cf63e09bcfb1d2
Дмитрий Васильев <orckuss@gmail.com>
2021-10-07 22:54:20 +0300

    module3-task1: Add routing

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,13 +1,61 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import { Movie } from '../../types/film';
+import AddReview from '../pages/add-review/add-review';
+import Film from '../pages/film/film';
 import Main from '../pages/main/main';
+import MyList from '../pages/my-list/my-list';
+import NotFound from '../pages/not-found/not-found';
+import Player from '../pages/player/player';
+import SignIn from '../pages/sign-in/sign-in';
+import { AppRoutes } from './routes';
 
 type Props = {
   promoFilm: Omit<Movie, 'id' | 'previewImage' | 'isFavorite'>;
 }
 
 function App({ promoFilm }: Props): JSX.Element {
+  const {
+    name,
+    backgroundImage,
+    posterImage,
+  } = promoFilm;
+
   return (
-    <Main promoFilm={promoFilm} />
+    <BrowserRouter>
+      <Switch>
+        <Route path={AppRoutes.Main} exact>
+          <Main promoFilm={promoFilm} />
+        </Route>
+
+        <Route path={AppRoutes.SignIn} exact>
+          <SignIn />
+        </Route>
+
+        <Route path={AppRoutes.MyList} exact>
+          <MyList />
+        </Route>
+
+        <Route path={AppRoutes.Films} exact>
+          <Film movie={promoFilm} />
+        </Route>
+
+        <Route path={AppRoutes.Review} exact>
+          <AddReview
+            name={name}
+            backgroundImage={backgroundImage}
+            posterImage={posterImage}
+          />
+        </Route>
+
+        <Route path={AppRoutes.Player} exact>
+          <Player />
+        </Route>
+
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- project/src/components/pages/not-found/not-found.tsx
+++ project/src/components/pages/not-found/not-found.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './not-found.css';
 
 function NotFound(): JSX.Element {
@@ -11,7 +12,9 @@ function NotFound(): JSX.Element {
 
       <h1 className="not-found__title">404</h1>
       <p className="not-found__text">Страница не найдена</p>
-      <a className="btn not-found__link">Вернуться на главную</a>
+      <Link className="btn" to="/">
+        Вернуться на главную
+      </Link>
     </div>
   );
 }
