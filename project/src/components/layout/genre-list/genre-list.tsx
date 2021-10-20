20d1c9f3e6cb6742a9cd2e9b9350e3b0ab0de562
Дмитрий Васильев <orckuss@gmail.com>
2021-10-20 20:28:14 +0300

    module6-task1: Connect global state to genre-list component

--- project/src/components/layout/genre-list/genre-list.tsx
+++ project/src/components/layout/genre-list/genre-list.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
 import { DEFALUT_ACTIVE_GENRE } from '../../../constants';
 import { GENRES } from '../../../mocks/genres';
-import { Genre } from '../../../types/genre';
+import { connect, ConnectedProps } from 'react-redux';
+import { GlobalState } from '../../../types/global-state';
+import { bindActionCreators, Dispatch } from 'redux';
+import { Actions } from '../../../types/actions';
+import { changeGenre } from '../../../store/actions';
 
-function GenreList(): JSX.Element {
-  const [activeGenre, setActiveGenre] = useState<Genre>('All genres');
+const mapStateToProps = ({ genre }: GlobalState) => ({
+  activeGenre: genre,
+});
 
+const mapDispatchToProps = (dispatch: Dispatch<Actions>) => bindActionCreators({
+  onChangeGenre: changeGenre,
+}, dispatch);
+
+const connector = connect(mapStateToProps, mapDispatchToProps);
+
+type PropsFromRedux = ConnectedProps<typeof connector>
+
+function GenreList({ activeGenre, onChangeGenre }: PropsFromRedux): JSX.Element {
   return (
     <ul className="catalog__genres-list">
       <li className={`catalog__genres-item ${activeGenre === DEFALUT_ACTIVE_GENRE && 'catalog__genres-item--active'}`}>
         <a
           href="/"
           className="catalog__genres-link"
-          onClick={(evt) => { evt.preventDefault(); setActiveGenre(DEFALUT_ACTIVE_GENRE); }}
+          onClick={(evt) => { evt.preventDefault(); onChangeGenre(DEFALUT_ACTIVE_GENRE); }}
         >
           All genres
         </a>
@@ -26,7 +39,7 @@ function GenreList(): JSX.Element {
           <a
             href="/"
             className="catalog__genres-link"
-            onClick={(evt) => { evt.preventDefault(); setActiveGenre(genre); }}
+            onClick={(evt) => { evt.preventDefault(); onChangeGenre(genre); }}
           >
             {genre}
           </a>
@@ -36,4 +49,5 @@ function GenreList(): JSX.Element {
   );
 }
 
-export default GenreList;
+export { GenreList };
+export default connector(GenreList);
