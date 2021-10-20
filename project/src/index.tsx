b20ff599adaf682239c49f18196b8f668bbb3f79
Дмитрий Васильев <orckuss@gmail.com>
2021-10-20 12:09:09 +0300

    module6-task1: Create reduser and store

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import App from './components/app/app';
 import { FILMS_MOCK } from './mocks/films';
 import { PROMO_FILM_MOCK } from './mocks/promo-film';
+import { reducer } from './store/reducer.';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(reducer, composeWithDevTools());
 
 ReactDOM.render(
   <React.StrictMode>
-    <App
-      promoFilm={PROMO_FILM_MOCK}
-      films={FILMS_MOCK}
-    />
+    <Provider store={store}>
+      <App
+        promoFilm={PROMO_FILM_MOCK}
+        films={FILMS_MOCK}
+      />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'));
--- project/src/store/reducer..ts
+++ project/src/store/reducer..ts
@@ -1,4 +1,7 @@
+import { Reducer } from 'redux';
 import { FILMS_MOCK } from '../mocks/films';
+import { Actions } from '../types/actions';
+import { Genre } from '../types/genre';
 import { GlobalState } from '../types/global-state';
 
 export const initialState: GlobalState = {
@@ -6,5 +9,15 @@ export const initialState: GlobalState = {
   genre: {
     active: true,
     name: 'All genres',
-  },
+  } as Genre,
+};
+
+export const reducer: Reducer<GlobalState, Actions> = (
+  state: GlobalState = initialState,
+  action: Actions,
+): GlobalState => {
+  switch (action.type) {
+    default:
+      return state;
+  }
 };
