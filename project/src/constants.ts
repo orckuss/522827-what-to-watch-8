f8c66960c4f343d4b5e7e167f00502283e425845
Дмитрий Васильев <orckuss@gmail.com>
2021-11-01 01:44:38 +0300

    module7-task1: Create axios configure function

--- project/src/constants.ts
+++ project/src/constants.ts
@@ -13,6 +13,12 @@ export const STAR_COUNT = 10;
 
 export const DEFALUT_ACTIVE_GENRE: Genre = 'All genres';
 
+export const FILM_CARDS_COUNT = 8;
 export const SIMILAR_FILMS_COUNT = 4;
 
-export const FILM_CARDS_COUNT = 8;
+export const REQUEST_BASE_URL = 'https://8.react.pages.academy/wtw';
+export const REQUEST_TIMEOUT = 5000;
+
+export const enum HttpCode {
+  Unauthorized = 401,
+}
